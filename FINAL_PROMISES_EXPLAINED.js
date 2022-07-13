f92f48973bd679a38daa96dbd4407ce54f39cff5
devDogra <48864825+devDogra@users.noreply.github.com>
2022-07-14 00:50:47 +0530

    Create FINAL_PROMISES_EXPLAINED.js

---
+++ FINAL_PROMISES_EXPLAINED.js
@@ -0,0 +1,15 @@
+NEW PROMISE MENTAl MODEL
+const p = new Promise(function(resolve, reject){
+	if (...) resolve(xyz)
+	else reject(abc)
+})
+
+// p is a promise to run the function we give to the promise. resolve(xyz) means basically that xyz is the return value in case of success, and abc is return value in case of error/fail
+
+// promise p after running runs the function inside .then() in case of SUC and the one inside of .catch() in case of FAIL/ERROR
+// this function gets 1 arg, which is the value that was returned by the promise
+p.then(function(resolveArg){
+	..... do stuff with resolveArg // xyz
+}).catch(function(rejectArg){
+	..... do stuff with the error msg OR general error
+}).finally(function(){ // do whatever})
